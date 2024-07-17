26a2587f5932f77a7bf87157f68256c57bcc73a6
NoN Responsive WebDeveloper <gdbcodes@gmail.com>
2024-07-17 15:18:45 +0530

    project init

---
+++ public/javascripts/script.js
@@ -0,0 +1,77 @@
+function filefolderbanao(){
+var fileform=document.querySelector(".fileform");
+var file=document.querySelector(".ri-file-line");
+var folderform=document.querySelector(".folderform");
+var folder=document.querySelector(".ri-folder-add-line");
+var fileevent=0;
+var folderevent=0;
+
+var close=()=>{
+   document.querySelectorAll(".form").forEach((form)=>{
+    console.log(form);
+    form.style.display="none";
+
+   })
+     
+
+}
+
+
+file.addEventListener("click",()=>{
+    close();
+   
+    if(fileevent==0){
+        fileform.style.display="initial";
+        
+        fileevent=1;
+        
+
+    }
+    else{
+        fileform.style.display="none";
+       
+        fileevent=0;
+
+    }
+   
+
+})
+
+
+
+folder.addEventListener("click",()=>{
+    close();
+
+    if(folderevent==0){
+        folderform.style.display="initial";
+       
+        folderevent=1;
+    }
+    else{
+        folderform.style.display="none";
+       
+        folderevent=0;
+
+    }
+   
+
+})
+
+
+
+};
+
+filefolderbanao();
+
+
+var editbutton=document.querySelector(".editbutton");
+var popup=document.querySelector(".overlaymain");
+
+
+editbutton.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        popup.style.display="flex";
+        console.log("Hua");
+    })
+
+})
---
+++ routes/index.js
@@ -0,0 +1,135 @@
+const { log } = require("console");
+var express = require("express");
+var router = express.Router();
+
+var fs = require("fs");
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  fs.readdir("./files", { withFileTypes: true }, (err, data) => {
+    if (err) {
+      console.log("err");
+    }
+
+    res.render("index", { data });
+    // data.forEach(function(file){
+    //   console.log(file.name);
+    // })
+  });
+});
+
+
+//create file
+router.get("/filebanao", function (req, res, next) {
+  fs.writeFile(`./files/${req.query.filekanaam}`, "", (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("kaam hogya bhi ji apki file ban gyi hai");
+    }
+  });
+  res.redirect("back");
+});
+
+
+//create folder
+router.get("/folderbanao", function (req, res, next) {
+  fs.mkdir(`./files/${req.query.folderkanaam}`, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("kaam hogya bhi ji apki folder ban gyi hai");
+    }
+  });
+  res.redirect("back");
+});
+
+
+
+//delete file
+router.get("/deleteThisFile/:filename", (req, res) => {
+  fs.unlink(`./files/${req.params.filename}`, (err) => {
+    if (err) {
+      console.log(err,"err");
+    } else {
+      console.log("delete hogya");
+    }
+   
+  });
+  res.redirect("back");
+ 
+});
+
+
+//delete folder
+router.get("/deleteThisFolder/:filename", (req, res) => {
+  fs.rmdir(`./files/${req.params.filename}`, (err) => {
+    if (err) {
+      console.log(err,"err");
+    } else {
+      console.log("delete hogya");
+    }
+   
+  });
+  res.redirect("back");
+ 
+});
+
+
+
+
+
+
+router.get("/write/:filename", (req, res) => {
+
+
+
+
+  fs.readdir("./files", { withFileTypes: true }, (err, data) => {
+    if (err) {
+      console.log("err");
+    }
+
+    fs.readFile(`./files/${req.params.filename}`, function read(err, info) {
+      if (err) {
+          throw err;
+      }
+      
+     let textdata=info.toString()
+      
+      res.render("file",{data,filename:req.params.filename,textdata});
+  });
+
+
+    
+   
+  });
+
+  
+
+});
+
+
+
+
+
+
+
+router.post("/save/:filename", (req, res) => {
+
+  fs.writeFile(`./files/${req.params.filename}`, `${req.body.text}` , function(err) {
+    if(err) {
+        return console.log(err);
+    }
+
+
+    
+    
+    res.redirect("back");
+}); 
+
+
+
+});
+
+module.exports = router;
